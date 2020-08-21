612688780b44b4dac22d2aa8aadb12fdc1746987
Chris Hull <chris.f.hull@gmail.com>
2020-08-21 10:02:32 -0400

    Updates

---
+++ precache-manifest.98f04b2d2046d6de6e6e9040e1821fd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9676c19faef71372959a33b7e68ce155",
+    "url": "/eve-raiders-data/index.html"
+  },
+  {
+    "revision": "4c36b313dc0a5fdeab7c",
+    "url": "/eve-raiders-data/static/css/main.cc86864d.chunk.css"
+  },
+  {
+    "revision": "6232d7305654dd41432f",
+    "url": "/eve-raiders-data/static/js/2.8e00ba22.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/eve-raiders-data/static/js/2.8e00ba22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c36b313dc0a5fdeab7c",
+    "url": "/eve-raiders-data/static/js/main.82482605.chunk.js"
+  },
+  {
+    "revision": "c8446ea3a6d3ef04ddb4",
+    "url": "/eve-raiders-data/static/js/runtime-main.ac04dce6.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/eve-raiders-data/precache-manifest.98f04b2d2046d6de6e6e9040e1821fd7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/eve-raiders-data/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
