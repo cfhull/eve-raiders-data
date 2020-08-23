ac8ef1135949853dd2674fcba9df0e3024c65c9e
Chris Hull <chris.f.hull@gmail.com>
2020-08-23 17:11:35 -0400

    under development placeholder

---
+++ src/Dashboard/UnderDevelopment/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const UnderDevelopment = () => (
+  <div style={{ marginTop: 20 }}>
+    Feature currently under development. Come back later!
+  </div>
+);
+
+export default UnderDevelopment;
--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 import ResourceSearch from "./ResourceSearch";
+import UnderDevelopment from "./UnderDevelopment";
 import NavBar from "./NavBar";
 import styles from "./Dashboard.module.scss";
+
+const routes = {
+  "resource-search": ResourceSearch,
+  "*": UnderDevelopment,
+};
 
 const Dashboard = () => {
   const [selectedRoute, setSelectedRoute] = useState("resource-search");
 
+  const Component = routes[selectedRoute] || routes["*"];
+
   return (
     <div className={styles.dashboard}>
       <NavBar route={selectedRoute} setRoute={setSelectedRoute} />
-      {selectedRoute === "resource-search" && <ResourceSearch />}
+      <Component />
     </div>
   );
 };
