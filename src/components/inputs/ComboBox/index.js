f7cf535437edaaaf656a9d9b793b9838ac2b3f3b
Chris Hull <chris.f.hull@gmail.com>
2020-08-23 19:53:44 -0400

    remove unused var

--- src/components/inputs/ComboBox/index.js
+++ src/components/inputs/ComboBox/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useCombobox } from "downshift";
 import BaseInput from "../BaseInput";
 import classNames from "classnames";
